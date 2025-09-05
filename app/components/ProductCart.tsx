034a8e651600a4935fbd08e9b2abdf5e683bc767
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-09-05 22:13:35 +0530

    got cooked

---
+++ app/components/ProductCart.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const ProductCart = () => {
+  return (
+    <div>ProductCart <br />
+    
+    <button onClick={()=>{console.log('clicked')}}>add to cart</button></div>
+  )
+}
+
+export default ProductCart
